b85bdb38bf58b85cf47102fd9d4f4a5fce5141fc
heniardiana <heniardiana@gmail.com>
2020-08-04 08:45:40 +0700

    Tugas Niomic
    Tugas 22 Javascript niomic

---
+++ Tugas22.js
@@ -0,0 +1,12 @@
+function panggilForEach(){
+    var data = ['saya','ingin','belajar','bersama'];
+    data.forEach(function (item, index){
+        console.log("Item :",item,"Index ke",index);
+        
+    })
+}
+
+panggilForEach()
+
+
+
